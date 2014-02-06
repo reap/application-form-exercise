d92a288bc687aa895e57337ff6a7d4dc9959e8da
Ilari Liukko <ilari.liukko@iki.fi>
2014-02-06 06:30:20 +0200

    Renamed

--- src/main/java/com/github/reap/application/ApplicationFormUI.java
+++ src/main/java/com/github/reap/application/ApplicationFormUI.java
@@ -1,4 +1,4 @@
-package com.github.reap;
+package com.github.reap.application;
 
 import javax.servlet.annotation.WebServlet;
 
@@ -17,28 +17,24 @@ import com.vaadin.ui.VerticalLayout;
 
 @Theme("reindeer")
 @SuppressWarnings("serial")
-public class MyVaadinUI extends UI
-{
+public class ApplicationFormUI extends UI {
 
- 
     @WebServlet(value = "/*", asyncSupported = true)
-    @VaadinServletConfiguration(productionMode = false, ui = MyVaadinUI.class, widgetset = "com.github.reap.AppWidgetSet")
+    @VaadinServletConfiguration(productionMode = false, ui = ApplicationFormUI.class, widgetset = "com.github.reap.AppWidgetSet")
     public static class Servlet extends VaadinServlet {
     }
 
     @Override
     protected void init(VaadinRequest request) {
-        
+
         Injector injector = Guice.createInjector(new DatasourceModule(), new HibernateStorageModule(), new UiModule());
         ApplicationFormPresenter applicationPresenter = injector.getInstance(ApplicationFormPresenter.class);
-        
+
         final VerticalLayout layout = new VerticalLayout();
         layout.setMargin(true);
         setContent(layout);
 
         layout.addComponent(applicationPresenter.getView().getComponent());
     }
-
-
 
 }
